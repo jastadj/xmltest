4402754720e4d1707c240ce4d682adc5d5f92037
John J <jastadj@gmail.com>
2016-02-19 11:36:17 -0800

    wip2

--- main.cpp
+++ main.cpp
@@ -23,19 +23,77 @@ struct Album
 
 std::vector<Album> albums;
 
+bool promptyorn(std::string pstring)
+{
+    while(1)
+    {
+        std::string buf;
+
+        std::cout << pstring;
+
+        std::getline(std::cin, buf);
+
+        if(buf == "y" || buf == "Y" || buf == "yes" || buf == "YES") return true;
+        else if(buf == "n" || buf == "N" || buf == "no" || buf == "NO") return false;
+    }
+    return false;
+}
+
+void hitReturn(std::string str = std::string("Hit return to continue..."))
+{
+    std::string buf;
+    std::cout << str;
+    std::getline(std::cin, buf);
+}
+void printAlbum(Album *talbum)
+{
+    if(talbum == NULL)
+    {
+        std::cout << "Error : album is NULL\n";
+        return;
+    }
+
+    std::cout << "Album Title : " << talbum->albumName << std::endl;
+    std::cout << "Artist      : " << talbum->bandName << std::endl;
+    std::cout << "Release Year: " << talbum->year << std::endl;
+
+    std::cout << "\n";
+    std::cout << talbum->tracks.size() << " Tracks.\n";
+    for(int i = 0; i < int(talbum->tracks.size()); i++)
+    {
+        std::cout << "     " << i+1 << "." << talbum->tracks[i].trackName << std::endl;
+    }
+}
+
 bool addNewTrack(Album *talbum)
 {
     if(talbum == NULL) return false;
 
+    Track newtrack;
 
+    std::string buf;
+
+    std::cout << "\nEnter track name : ";
+    std::getline(std::cin, buf);
+    newtrack.trackName = buf;
+
+    std::cout << "Enter track length (sec) : ";
+    std::getline(std::cin, buf);
+    newtrack.lengthInSec = atoi(buf.c_str());
+
+    if(promptyorn("Add this track? (y/n)"))
+    {
+        talbum->tracks.push_back(newtrack);
+        return true;
+    }
+    else return false;
 }
 
 bool addNewAlbum()
 {
     Album newalbum;
     std::string buf;
-
-    system("cls");
+     system("cls");
 
     std::cout << "Enter Band Name : ";
     std::getline(std::cin, buf);
@@ -49,9 +107,25 @@ bool addNewAlbum()
     std::getline(std::cin, buf);
     newalbum.year = atoi(buf.c_str());
 
+    printAlbum(&newalbum);
 
+    while(promptyorn("Add new track? (y/n)"))
+    {
+        addNewTrack(&newalbum);
+    }
 
+    if(promptyorn("Save album? (y/n)"))
+    {
+        albums.push_back(newalbum);
+        return true;
+    }
+    else return false;
+}
 
+void printAlbumList()
+{
+    std::cout << "\nAlbums:\n";
+    for(int i = 0; i < int(albums.size()); i++) std::cout << i+1 << ". " << albums[i].albumName << std::endl;
 }
 
 void mainMenu()
@@ -67,6 +141,8 @@ void mainMenu()
 
         std::vector<std::string> menuitems;
         menuitems.push_back("New Album");
+        menuitems.push_back("List Albums");
+        menuitems.push_back("Show Album Details");
         menuitems.push_back("Quit");
 
         std::cout << "Total Albums In Database : " << albums.size() << "\n\n";
@@ -86,6 +162,35 @@ void mainMenu()
         if(selection < 0 || selection >= int(menuitems.size()) ) continue;
         else if(menuitems[selection] == "Quit") quit = true;
         else if(menuitems[selection] == "New Album") addNewAlbum();
+        else if(menuitems[selection] == "List Albums")
+        {
+            printAlbumList();
+            hitReturn();
+        }
+        else if(menuitems[selection] == "Show Album Details")
+        {
+            if(albums.empty())
+            {
+                hitReturn("Album list is empty!");
+                continue;
+            }
+
+                std::string abuf;
+                int abufnum = 0;
+                printAlbumList();
+                std::cout << "Show details for which album? [1-" << albums.size() << "] : ";
+                std::getline(std::cin, abuf);
+                abufnum = atoi(abuf.c_str())-1;
+
+                if(abufnum < 0 || abufnum >= int(albums.size()) )
+                {
+                    hitReturn("Not a valid album #!");
+                    continue;
+                }
+
+                printAlbum(&albums[abufnum]);
+                hitReturn();
+        }
     }
 
 }
